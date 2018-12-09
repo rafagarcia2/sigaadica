fe79013d0d381bd8453b02830a28fa7a404165a9
Gabriel <gabrieldiniz54@gmail.com>
2018-12-09 08:56:08 -0200

    Iniciadas rotas dos cursos

--- home/serializers.py
+++ home/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Departamento
+from .models import Departamento, Curso
 
 
 class DepartamentoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Departamento
+        fields = ('pk', 'codigo', 'nome', 'ativo', 'data_criacao')
+
+
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
         fields = ('pk', 'codigo', 'nome', 'ativo', 'data_criacao')
--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
-from rest_framework import viewsets
-from .serializers import DepartamentoSerializer
-from .models import Departamento
+from django.core.exceptions import ObjectDoesNotExist
+
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import DepartamentoSerializer, CursoSerializer
+from .models import Departamento, Curso
 
 
 class DepartamentoViewSet(viewsets.ModelViewSet):
@@ -9,3 +14,37 @@ class DepartamentoViewSet(viewsets.ModelViewSet):
     """
     queryset = Departamento.objects.all().order_by('-nome')
     serializer_class = DepartamentoSerializer
+
+
+
+### CURSOS ###
+
+@api_view(['GET'])
+def curso(request):
+    """
+    View responsável por exibir todos os cursos
+    """
+    cursos_serializer = CursoSerializer(Curso.objects.all(), many=True)
+    return Response(cursos_serializer.data)
+
+
+@api_view(['GET'])
+def curso_id(request, id):
+    """
+    View responsável por devolver um curso específico pelo seu id
+    """
+    try:
+        curso = Curso.objects.get(id=id)
+        curso_serialzer = CursoSerializer(curso)
+
+        return Response(curso_serialzer.data)
+    except ObjectDoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET'])
+def curso_por_departamento(request, id):
+    """
+    View responsável por pegar todos os cursos de um determinado departamento
+    """
+    pass
