cb9a558a93ff23ccad25f0c77a8bbfdcb84429f4
Gabriel <gabrieldiniz54@gmail.com>
2018-12-12 20:03:21 -0200

    Começo das rotas das disciplinas

--- home/views.py
+++ home/views.py
@@ -1,12 +1,13 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 
 from rest_framework import viewsets, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
-from .serializers import DepartamentoSerializer, CursoSerializer
-from .models import Departamento, Curso
+from .serializers import DepartamentoSerializer, CursoSerializer, DisciplinaSerializer
+from .models import Departamento, Curso, Disciplina
 
 
 class DepartamentoViewSet(viewsets.ModelViewSet):
@@ -47,3 +48,42 @@ def curso_por_departamento(request, id):
     Recupera todos os cursos de um determinado departamento
     """
     pass
+
+
+
+### DISCIPLINA ###
+
+
+@api_view(['GET'])
+def disciplinas(request):
+    """
+    Recupera todas as disciplinas cadastradas
+    """
+    disciplinas = Disciplina.objects.all()
+    disciplinas_serialized = DisciplinaSerializer(disciplinas, many=True)
+
+    return Response(disciplinas_serialized.data)
+
+
+@api_view(['GET'])
+def disciplina_id(request, id):
+    """
+    Recupera uma disciplina específica pelo seu id
+    """
+    disciplina = get_object_or_404(Disciplina, pk=id)
+    disciplina_serialized = DisciplinaSerializer(disciplina)
+
+    return Response(disciplina_serialized.data)
+
+
+@api_view(['GET'])
+def pesquisa_disciplina(request):
+    """
+    Recupera uma disciplina de acordo com seu nome ou código
+    """
+    pesquisa = request.GET.get('nome')
+
+    disciplina = get_object_or_404(Disciplina, Q(nome__icontains=pesquisa) | Q(codigo=pesquisa))
+    disciplina_serialized = DisciplinaSerializer(disciplina)
+
+    return Response(disciplina_serialized.data)
