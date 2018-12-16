6e095553179c4b0da8a39ca3e141aceeb3afc116
HemersonRafael <rafaelpontes1995@gmail.com>
2018-12-16 00:35:09 -0300

    add new models,serializers, views and urls

--- home/models.py
+++ home/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 
-
 class Professor(models.Model):
-    codigo = models.IntegerField('Código')
-    nome = models.CharField('Nome', max_length=100)
-    vinculo = models.CharField('Vínculo', max_length=100)
     
-    ativo = models.BooleanField('Ativo', default=True)
-    data_criacao = models.DateTimeField('Criado em', auto_now_add=True)
+    codigo = models.CharField('Código',max_length=100)
+    nome = models.CharField('Nome',max_length=100)    
+    ativo = models.BooleanField('Ativo',default=True)
+    data_criacao = models.DateTimeField('Criado em',auto_now_add=True)
 
     def __str__(self):
         return self.nome
@@ -17,13 +15,11 @@ class Professor(models.Model):
         verbose_name_plural = 'Professores'
         ordering = ('nome', 'codigo')
 
-
 class Departamento(models.Model):
-    codigo = models.IntegerField('codigo')
-    nome = models.CharField('Nome', max_length=100)
-    
-    ativo = models.BooleanField('Ativo', default=True)
-    data_criacao = models.DateTimeField('Criado em', auto_now_add=True)
+    codigo = models.CharField('Código',max_length=100)
+    nome = models.CharField('Nome',max_length=100)    
+    ativo = models.BooleanField('Ativo',default=True)
+    data_criacao = models.DateTimeField('Criado em',auto_now_add=True)
 
     def __str__(self):
         return self.nome
@@ -33,13 +29,16 @@ class Departamento(models.Model):
         verbose_name_plural = 'Departamentos'
         ordering = ('nome', 'codigo')
 
-
 class Curso(models.Model):
-    codigo = models.IntegerField('codigo')
-    nome = models.CharField('Nome', max_length=100)
-    
-    ativo = models.BooleanField('Ativo', default=True)
-    data_criacao = models.DateTimeField('Criado em', auto_now_add=True)
+    departamento = models.ForeignKey(
+        Departamento, 
+        verbose_name='Departamento', 
+        on_delete=models.CASCADE
+    )
+    codigo = models.CharField('Código',max_length=100)
+    nome = models.CharField('Nome',max_length=100)    
+    ativo = models.BooleanField('Ativo',default=True)
+    data_criacao = models.DateTimeField('Criado em',auto_now_add=True)
 
     def __str__(self):
         return self.nome
@@ -49,15 +48,16 @@ class Curso(models.Model):
         verbose_name_plural = 'Cursos'
         ordering = ('nome', 'codigo')
 
-
 class Disciplina(models.Model):
-    id_componente = models.IntegerField('id_componente')
-    nome = models.CharField('Nome', max_length=200)
-    codigo = models.CharField('codigo', max_length=200)
-    departamento = models.CharField('Departamento', max_length=200)
-
-    ativo = models.BooleanField('Ativo', default=True)
-    data_criacao = models.DateTimeField('Criado em', auto_now_add=True)
+    departamento = models.ForeignKey(
+        Departamento,
+        verbose_name='Departamento', 
+        on_delete=models.CASCADE
+    )
+    codigo = models.CharField('Código',max_length=100)
+    nome = models.CharField('Nome',max_length=100)    
+    ativo = models.BooleanField('Ativo',default=True)
+    data_criacao = models.DateTimeField('Criado em',auto_now_add=True)
 
     def __str__(self):
         return self.nome
@@ -65,46 +65,27 @@ class Disciplina(models.Model):
     class Meta:
         verbose_name = 'Disciplina'
         verbose_name_plural = 'Disciplinas'
-        ordering = ('departamento', 'nome', 'id_componente',)
-
-
-class Avaliacao(models.Model):
-    codigo = models.IntegerField('codigo')
-    turma = models.CharField('Turma', max_length=100)
-    postura_profissional_media = models.DecimalField('Média da Postura Profissional', decimal_places=2, max_digits=5)
-    postura_profissional_dp = models.DecimalField('Desvio Padrão da Postura Profissional do docente', decimal_places=2, max_digits=5)
-    atuacao_profissional_media = models.DecimalField('Média da Atuacação Profissional do docente', decimal_places=2, max_digits=5)
-    atuacao_profissional_dp = models.DecimalField('Desvio Padrão da Atuação Profissional do docente', decimal_places=2, max_digits=5)
-    autoavaliacao_aluno_media = models.DecimalField('Média da Postura Profissional do docente', decimal_places=2, max_digits=5)
-    autoavaliacao_aluno_dp = models.DecimalField('Desvio Padrão da Postura Profissional', decimal_places=2, max_digits=5)
-
-    ativo = models.BooleanField('Ativo', default=True)
-    data_criacao = models.DateTimeField('Criado em', auto_now_add=True)
-
-    def __str__(self):
-        return '{} - {}'.format(self.codigo, self.turma)
-    
-    class Meta:
-        verbose_name = 'Avaliação'
-        verbose_name_plural = 'Avaliações'
-        ordering = ('codigo', 'turma',)
-
+        ordering = ('departamento', 'nome','codigo')
 
 class Turma(models.Model):
-    codigo = models.IntegerField('codigo')
-    disciplina = models.ForeignKey(Disciplina, verbose_name='Disciplina', on_delete=models.CASCADE)
-    professor = models.ForeignKey(Professor, verbose_name='Professor', on_delete=models.CASCADE)
-    qnt_discentes = models.IntegerField('Quantidade de Discentes')
-
-    taxa_aprovacao = models.DecimalField('Taxa de Aprovação', decimal_places=2, max_digits=5)
-    media_turma = models.DecimalField('Média da Turma', decimal_places=2, max_digits=5)
-    qnt_aprovados_primeira = models.DecimalField('Quantidade de Aprovados na primeira vez que pagou', decimal_places=2, max_digits=5)
-    evasao = models.DecimalField('Quantidade de Trancamentos', decimal_places=2, max_digits=5)
-    autoavaliacao_aluno_media = models.DecimalField('Média da Postura Profissional do docente', decimal_places=2, max_digits=5)
-    autoavaliacao_aluno_dp = models.DecimalField('Desvio Padrão da Postura Profissional', decimal_places=2, max_digits=5)
-
-    ativo = models.BooleanField('Ativo', default=True)
-    data_criacao = models.DateTimeField('Criado em', auto_now_add=True)
+    professor = models.ForeignKey(
+        Professor, 
+        verbose_name='Professor', 
+        on_delete=models.CASCADE
+    )
+    disciplina = models.ForeignKey(
+        Disciplina, 
+        verbose_name='Disciplina', 
+        on_delete=models.CASCADE
+    )
+    codigo = models.CharField('Código',max_length=100)
+    qnt_discentes = models.IntegerField('Quantidade de discentes', default = 0)
+    tx_aprov = models.FloatField('Taxa de aprovação', default = 0.0)
+    media_turma = models.FloatField('Média da turma', default = 0.0)
+    qnt_tranc = models.IntegerField('Quantidade de trancamentos', default = 0)
+    aprov_prim = models.IntegerField('Aprovados de aprovados na primeira vez que pagou', default = 0)
+    ativo = models.BooleanField(default=True)
+    data_criacao = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return '{} - {}'.format(self.codigo, self.disciplina)
@@ -112,4 +93,25 @@ class Turma(models.Model):
     class Meta:
         verbose_name = 'Turma'
         verbose_name_plural = 'Turmas'
-        ordering = ('disciplina', 'codigo',)
+        ordering = ('professor','disciplina', 'codigo',)
+
+class Avaliacao(models.Model):
+    turma = models.ForeignKey(
+        Turma, 
+        
+        verbose_name='Turma', 
+        on_delete=models.CASCADE
+    )
+    codigo = models.CharField('Código',max_length=100)
+    auto_avaliacao = models.FloatField('Auto avaliação', default = 0.0) 
+    post_prof = models.FloatField('Postura profissional', default = 0.0)
+    ativo = models.BooleanField('Ativo',default=True)
+    data_criacao = models.DateTimeField('Criado em',auto_now_add=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.codigo, self.turma)
+    
+    class Meta:
+        verbose_name = 'Avaliação'
+        verbose_name_plural = 'Avaliações'
+        ordering = ('turma','codigo',)
