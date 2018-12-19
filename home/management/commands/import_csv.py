b89fe15e9a44363967f3179bb65a62c6c8dd2ebe
Ítalo Epifânio <italoepifaniols@gmail.com>
2018-12-18 21:42:01 -0300

    Avanços na importação de turmas

--- home/management/commands/import_csv.py
+++ home/management/commands/import_csv.py
@@ -2,6 +2,8 @@ from django.core.management.base import BaseCommand, CommandError
 from import_export import resources
 from home.models import Professor, Disciplina, Turma
 from tablib import Dataset
+from import_export.fields import Field
+from import_export.widgets import ForeignKeyWidget
 import os
 
 class DisciplinaResource(resources.ModelResource):
@@ -19,8 +21,24 @@ class ProfessorResource(resources.ModelResource):
         return False
 
 class TurmaResource(resources.ModelResource):
+    # disciplina_id = Field(
+    #     column_name='id_componente',
+    #     attribute='disciplina_id',
+    #     widget=ForeignKeyWidget(Disciplina, 'id_componente')
+    # )
+
+    disciplina_id = Field(
+        column_name='id_componente',
+        attribute='disciplina',
+        widget=ForeignKeyWidget(Disciplina, 'id_componente')
+    )
+
     class Meta:
         model = Turma
+
+        # fields = ('disciplina__id_componente',)
+        skip_unchanged = True
+        report_skipped = False
 
     def get_instance(self, instance_loader, row):
         return False
@@ -35,18 +53,22 @@ class Command(BaseCommand):
         path = os.path.abspath('./data')
 
         if os.path.exists(path):
-            csvs = os.listdir(path)
-
+            # csvs = os.listdir(path)
+            csvs = ['professor.csv', 'disciplina.csv', 'turma.csv']
+            # csvs = ['turma.csv']
             for csv_file in csvs:
                 path_file = os.path.join(path, csv_file)
+
+                # if csv_file == 'turma.csv':
+                #     print(open(path_file).read())
 
                 dataset = Dataset()
 
                 dataset.load(open(path_file).read())
-
+                print(dataset)
                 csv_class = globals()[csv_file.split('.')[0].capitalize() + 'Resource']
                 csv_object = csv_class()
-                result = csv_object.import_data(dataset, dry_run=True)
+                result = csv_object.import_data(dataset, dry_run=True, raise_errors=True)
 
                 if not result.has_errors():
                     csv_class().import_data(dataset, dry_run=False)
